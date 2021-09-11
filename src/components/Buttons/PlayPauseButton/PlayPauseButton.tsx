5b48818314a844eeeaddff5fb369d9594a476537
Yotam Aran <yotam.sound@gmail.com>
2021-09-11 15:22:57 +0300

    modifie Button exoprts created PlayPauseButton

---
+++ src/components/Buttons/PlayPauseButton/PlayPauseButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Button } from '../Button';
+
+export class PlayPauseButton extends React.Component {
+  render() {
+    return <div>PlayPauseButton</div>;
+  }
+}
