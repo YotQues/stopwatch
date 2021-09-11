feee02c75853dc1fa9591d1792dc477a254f1af5
Yotam Aran <yotam.sound@gmail.com>
2021-09-11 15:18:57 +0300

    fast refresh fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+
 import App from './App';
+
+// 👇 A fix for fast refresh issue
+if (module.hot) module.hot.accept();
 
 ReactDOM.render(
   <React.StrictMode>
