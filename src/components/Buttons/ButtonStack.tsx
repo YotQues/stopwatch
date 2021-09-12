5df1524d73a21bad6ac298b8209ee3766680c153
Yotam Aran <yotam.sound@gmail.com>
2021-09-12 23:13:02 +0300

    button stack resonsive

--- src/components/Buttons/ButtonStack.tsx
+++ src/components/Buttons/ButtonStack.tsx
@@ -44,7 +44,7 @@ export function ButtonStack({
   };
 
   return (
-    <div className="flex gap-x-0 5">
+    <div className="flex gap-x-2 md:gap-x-3.5 lg:gap-x-5">
       {renderRunButton()}
       <LapButton onClick={onLapClick} />
       <ResetButton onClick={onResetClick} />
@@ -52,71 +52,3 @@ export function ButtonStack({
     </div>
   );
 }
-
-// const renderRunButton = (): JSX.Element => {
-//   if (state.isRun)
-//     return (
-//       <Button
-//         styles={{
-//           bgColor: 'green-400',
-//           activeBgColor: 'green-500',
-//           textColor: 'gray-200',
-//           activeTextColor: 'gray-100',
-//         }}
-//         onClick={onStartClick}
-//         label="Start"
-//       >
-//         <Play
-//           styles={{
-//             color: 'gray-200',
-//             activeColor: 'gray-100',
-//             width: '4/5',
-//           }}
-//         />
-//       </Button>
-//     );
-//   return (
-//     <Button
-//       styles={{
-//         bgColor: 'yellow-300',
-//         activeBgColor: 'yellow-400',
-//         textColor: 'gray-900',
-//         activeTextColor: 'gray-800',
-//       }}
-//       onClick={onPauseClick}
-//       label="Pause"
-//     >
-//       <Pause
-//         styles={{
-//           color: 'gray-900',
-//           activeColor: 'gray-800',
-//           width: '4/5',
-//         }}
-//       />
-//     </Button>
-//   );
-// };
-
-// return (
-//   <div className="flex gap-x-0.5">
-//     {renderRunButton()}
-//     <Button
-//       label="Lap"
-//       styles={{
-//         bgColor: 'gray-700',
-//         activeBgColor: 'gray-800',
-//         textColor: 'gray-200',
-//         activeTextColor: 'white',
-//       }}
-//       onClick={onLapClick}
-//     >
-//       <PlusCircleIcon
-//         styles={{
-//           color: 'gray-200',
-//           activeColor: 'white',
-//           width: '4/5',
-//         }}
-//       />
-//     </Button>
-//   </div>
-// );
